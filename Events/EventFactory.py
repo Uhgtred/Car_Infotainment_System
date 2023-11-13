01c8b8bcf8b50730fe4a13589589f927db481020
Uhgtred <Markus_koesters@gmx.de>
2023-11-13 20:42:56 +0100

    interface should not keep track of anything
    so now the factory does.

--- Events/EventFactory.py
+++ Events/EventFactory.py
@@ -9,6 +9,22 @@ class EventFactory:
     Factory-class for EventUser.
     """
 
-    @staticmethod
-    def produceEvent():
-        return Event()
+    __events: dict = {}
+
+    @classmethod
+    def produceEvent(cls, name: str) -> Event:
+        """
+        Method producing a new event.
+        :return:
+        """
+        if name not in cls.__events:
+            cls.__events[name].append(Event())
+        return cls.__events.get(name)
+
+    @property
+    def getEventsList(self) -> list[str]:
+        """
+        Getter Method for Events available.
+        :return: List of available Events.
+        """
+        return list(self.__events.keys())
--- Events/EventInterface.py
+++ Events/EventInterface.py
@@ -6,40 +6,22 @@ from Events.EventFactory import EventFactory
 
 class EventInterface:
 
-    __events: dict = {}
-
-    def createNewEvent(self, name: str) -> __events:
-        """
-        Method for creating a new event.
-        :param name: Clear name of the event. Existing events can be received by calling getEventsList.
-        :return: List of available Events.
-        """
-        instance = EventFactory.produceEvent()
-        self.__events[name].append(instance)
-        return list(self.__events.keys())
-
-    @property
-    def getEventsList(self) -> list[str]:
-        """
-        Getter Method for Events available.
-        :return: List of available Events.
-        """
-        return list(self.__events.keys())
-
-    def subscribeToEvent(self, eventName: str, callbackMethod: callable) -> None:
+    @staticmethod
+    def subscribeToEvent(eventName: str, callbackMethod: callable) -> None:
         """
         Method for subscribing to an existing Event.
         :param eventName: Clear name of the event. Existing events can be received by calling getEventsList.
         :param callbackMethod: Method that is going to be used to call back when event-update occurs.
         """
-        if eventName in self.__events:
-            self.__events.get(eventName).subscribe(callbackMethod=callbackMethod)
+        event = EventFactory.produceEvent(name=eventName)
+        event.subscribe(callbackMethod=callbackMethod)
 
-    def postEventUpdate(self, eventName: str, data: any) -> None:
+    @staticmethod
+    def postEventUpdate(eventName: str, data: any) -> None:
         """
         Method for notifying subscribers of an event.
         :param eventName: Clear name of the event. Existing events can be received by calling getEventsList.
         :param data: Data that will be shared to the subscribers.
         """
-        if eventName in self.__events:
-            self.__events.get(eventName).notifySubscribers(data=data)
+        event = EventFactory.produceEvent(name=eventName)
+        event.notifySubscribers(data=data)
