3de7e6620416b64560e5e3bc18c52fda6ec5062b
Uhgtred <markus_koesters@gmx.de>
2023-12-19 16:03:11 +0100

    Update test_EventFactory.py

--- Events/UnitTests/test_EventFactory.py
+++ Events/UnitTests/test_EventFactory.py
@@ -3,27 +3,15 @@
 
 import unittest
 
+from Events.EventFactory import EventFactory
 
 
 class MyTestCase(unittest.TestCase):
 
-    factory = ProduceEventUser()
-    testData = 'Test'
-    receivedData = None
-
-    def test_subscribeToEvent(self):
-        self.factory.subscribeToEvent(self.postEventUpdateHelper)
-        assert 'postEventUpdateHelper' in str(self.factory._ProduceEventUser__event._Event__subscribers)
-
-    def test_postEventUpdate(self):
-        self.factory.subscribeToEvent(self.postEventUpdateHelper)
-        # print(self.factory._ProduceEventUser__event._Event__subscribers)
-        self.factory.postEventUpdate(self.testData)
-        assert self.receivedData == self.testData
-
-    def postEventUpdateHelper(self, data):
-        self.receivedData = data
-
+    """
+    TODO: Implement this test here
+    """
+    pass
 
 if __name__ == '__main__':
     unittest.main()
