17275135ff32c8198efe80ac82a763f34fae0372
Uhgtred <Markus_koesters@gmx.de>
2023-12-19 21:28:58 +0100

    Update test_Logger.py

--- Logging/test_UnitTests/test_Logger.py
+++ Logging/test_UnitTests/test_Logger.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 # @author      Markus Kösters
+import os
 import time
 import unittest
 
@@ -7,18 +8,17 @@ from Logging.Logger import Logger
 
 
 class MyTestCase(unittest.TestCase):
-
     logger = Logger()
+    basePath = os.path.dirname(__file__)
 
     def test_createLogEntry(self):
         testDict = {'1stTest': 'info', '2ndTest': 'info', '3rdTest': 'warning'}
         for key in testDict:
             self.logger.createLogEntry(testDict.get(key), __name__, key)
-        with open('test_Logger.log', 'r') as logFile:
+        with open(os.path.join(self.basePath, 'test_Logger.log'), 'r') as logFile:
             lines = logFile.readlines()
         for line in lines:
             assert 'Test' in line
-
 
 
 if __name__ == '__main__':
