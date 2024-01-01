8c79c69eb055efcdf025443926d07b244fbca700
Uhgtred <Markus_koesters@gmx.de>
2024-01-01 22:47:39 +0100

    unit-tests
    starting implementation of unit-tests for socket-modules

---
+++ BusTransactions/Buses/Ethernet/test_UnitTests/SocketMock.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# @author: Markus Kösters
+
+class MockSocket:
+    buffer = []
+
+    def read(self):
+        if self.buffer:
+            return self.buffer.pop(0)
+
+    def write(self, message):
+        self.buffer.append(message)
+
+    @property
+    def getBuffer(self):
+        return self.buffer
