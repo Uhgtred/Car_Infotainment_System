8dd9141183f8740398ced1278d72568c7240a3a9
Uhgtred <Markus_koesters@gmx.de>
2024-01-02 20:44:46 +0100

    Update SocketMock.py

--- BusTransactions/Buses/Ethernet/test_UnitTests/SocketMock.py
+++ BusTransactions/Buses/Ethernet/test_UnitTests/SocketMock.py
@@ -3,10 +3,17 @@
 
 class MockSocket:
     buffer = []
+    state = False
 
     def read(self):
         if self.buffer:
             return self.buffer.pop(0)
+
+    def socket(self):
+        if self.state:
+            self.state = True
+        else:
+            self.state = False
 
     def write(self, message):
         self.buffer.append(message)
