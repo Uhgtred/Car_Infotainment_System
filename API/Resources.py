762e64fddc4acfbbf4c15e09da573a22e696053a
Uhgtred <Markus_koesters@gmx.de>
2024-01-09 22:24:18 +0100

    Update Resources.py
    either have to use a custom json serializer or figure some other way to open the udp-connection.

--- API/Resources.py
+++ API/Resources.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # @author: Markus Kösters
-from flask import Flask
+from flask import Flask, jsonify
 from flask_restful import Resource, Api
 
 from BusTransactions import BusInterfaceFactory
@@ -8,12 +8,14 @@ from BusTransactions import BusInterfaceFactory
 app = Flask('InfotainmentAPI')
 api = Api(app)
 
+
 class SocketRequest(Resource):
 
-    def get(self, port: int):
-        return BusInterfaceFactory.produceUDP_Transceiver(port)
+    def get(self, port: int) -> object:
+        return jsonify(BusInterfaceFactory.produceUDP_Transceiver(port))
 
-api.add_resource(SocketRequest, '/getSocketObject')
+
+api.add_resource(SocketRequest, '/getSocketObject/<int:port>')
 
 if __name__ == '__main__':
     app.run(debug=True)
